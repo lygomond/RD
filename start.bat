ffe5a5a705d812d45ef28e7af5e7e87336abc485
Lygo xd <31230364+lygomond@users.noreply.github.com>
2021-08-02 12:45:13 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -16,7 +16,7 @@ echo  Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @LygoRDP21
+echo Password: @Lygomond
 echo .
 echo  RDP By Lygo Mond (+_+)
 echo   Please Login to Your RDP !

5ea612d9643f5105781f01950728eb2290c5a34c
Lygo xd <31230364+lygomond@users.noreply.github.com>
2021-08-04 11:40:14 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,7 +4,7 @@ net config server /srvcomment:"Windows Server 2019 By LygoMond" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user administrator @Lygomond /add >nul
-net localgroup user administrator /add >nul
+net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
@@ -18,6 +18,6 @@ tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -
 echo Username: administrator
 echo Password: @Lygomond
 echo .
-echo  RDP By Lygo Mond (+_+)
+echo  RDP By LygoMond (+_+)
 echo   Please Login to Your RDP !
 ping -n 10 127.0.0.1 >nul
